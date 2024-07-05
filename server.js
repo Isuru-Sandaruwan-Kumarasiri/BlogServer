7901eec87746d88588e72fdfb2087f9280ca5fae
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2024-07-06 02:56:30 +0530

    set api blog interaction

--- server.js
+++ server.js
@@ -389,19 +389,19 @@ server.get('/trending-blogs',(req,res)=>{
 
 server.post("/search-blogs",(req,res)=>{
 
-    let {tag,query,author,page}=req.body;
+    let {tag,query,author,page,limit,eliminate_blog}=req.body;
 
     let findQuery;
 
     if(tag){
-        findQuery={tags:tag,draft:false};
+        findQuery={tags:tag,draft:false,blog_id:{$ne:eliminate_blog}};//dant tiyena blog id eka natuwa tag[0] blog ekak seveema($ne=Not equal)
     }else if(query){
         findQuery={draft:false,title:new RegExp(query,'i')};
     }else if(author){
         findQuery={author,draft:false};
     }
 
-    let maxLimit=2;
+    let maxLimit=limit ? limit :2;
 
     Blog.find(findQuery)
     .populate("author"," personal_info.profile_img personal_info.username personal_info.fullname -_id")
